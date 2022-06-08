cdae684b16089941dac742a8d6d51b9c2a91d185
Aaron Chris Deloso <deloso.aaronchris@auf.edu.ph>
2022-06-08 22:16:59 +0800

    fix 5 errors in AnimalViewModel

--- PracticalProject_Deloso_Nakpil_Miranda/PracticalProject_Deloso_Nakpil_Miranda/Services/DatabaseContext.cs
+++ PracticalProject_Deloso_Nakpil_Miranda/PracticalProject_Deloso_Nakpil_Miranda/Services/DatabaseContext.cs
@@ -6,22 +6,20 @@ using Microsoft.EntityFrameworkCore;
 using PracticalProject_Deloso_Nakpil_Miranda.Model;
 using System.IO;
 
-
 namespace PracticalProject_Deloso_Nakpil_Miranda.Services
 {
-    public class DatabaseContext : DbContext
+    public class DatabaseContext: DbContext
     {
-        public DbSet<DatabaseContext> Animals { get; set; }
+        public DbSet<AnimalModel> Animals { get; set; }
 
         public DatabaseContext()
         {
             this.Database.EnsureCreated();
         }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuiler)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "Animal.db");
-            optionsBuiler.UseSqlite($"Filename={dbPath}");
+            optionsBuilder.UseSqlite($"Filename={dbPath}");
         }
     }
 }
